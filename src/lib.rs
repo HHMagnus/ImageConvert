d8792fc7d136dd468414b55ce8b627d209dea41f
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2025-09-12 07:25:08 +0200

    added exposed function

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use std::io::Cursor;
 
 use image::{ImageError, ImageFormat};
+use wasm_bindgen::prelude::wasm_bindgen;
 
 fn map_image_err(err: ImageError) -> String {
     format!("Image processing error: {}", err)
@@ -11,4 +12,19 @@ fn convert(image_data: Vec<u8>, output: ImageFormat) -> Result<Vec<u8>, String> 
     let mut output_data: Vec<u8> = Vec::new();
     img.write_to(&mut Cursor::new(&mut output_data), output).map_err(map_image_err)?;
     Ok(output_data)
+}
+
+#[wasm_bindgen]
+pub fn convert_exposed(image_data: Vec<u8>, output: String) -> Result<Vec<u8>, String> {
+    let output = match output.to_lowercase().as_str() {
+        "png" => ImageFormat::Png,
+        "jpeg" | "jpg" => ImageFormat::Jpeg,
+        "gif" => ImageFormat::Gif,
+        "bmp" => ImageFormat::Bmp,
+        "ico" => ImageFormat::Ico,
+        "tiff" => ImageFormat::Tiff,
+        "webp" => ImageFormat::WebP,
+        _ => return Err("Unsupported output format".to_string()),
+    } ;
+    convert(image_data, output)
 }
