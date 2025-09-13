4a739a11f2ce6c50d563a172aa8da1494490d07c
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2025-09-13 13:54:25 +0200

    Added progress reporting from Rust

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,20 @@
 use std::io::Cursor;
 
 use image::{ImageError, ImageFormat};
-use wasm_bindgen::prelude::wasm_bindgen;
+use js_sys::Reflect;
+use wasm_bindgen::{prelude::wasm_bindgen, JsCast, JsValue};
 
 fn map_image_err(err: ImageError) -> String {
     format!("Image processing error: {}", err)
 }
 
 fn convert(image_data: Vec<u8>, output: ImageFormat) -> Result<Vec<u8>, String> {
+	report_progress("Loading image...");
     let img = image::load_from_memory(&image_data).map_err(map_image_err)?;
     let mut output_data: Vec<u8> = Vec::new();
+	report_progress("Converting to new format...");
     img.write_to(&mut Cursor::new(&mut output_data), output).map_err(map_image_err)?;
+	report_progress("Completed conversion.");
     Ok(output_data)
 }
 
@@ -27,4 +31,19 @@ pub fn convert_exposed(image_data: Vec<u8>, output: String) -> Result<Vec<u8>, S
         _ => return Err("Unsupported output format".to_string()),
     } ;
     convert(image_data, output)
+}
+
+fn report_progress(message: &str) {
+    let global = js_sys::global();
+
+    let func = js_sys::Reflect::get(&global, &JsValue::from_str("postMessage"))
+        .unwrap()
+        .dyn_into::<js_sys::Function>()
+        .unwrap();
+
+    let obj = js_sys::Object::new();
+    Reflect::set(&obj, &JsValue::from_str("type"), &JsValue::from_str("progress")).unwrap();
+    Reflect::set(&obj, &JsValue::from_str("message"), &JsValue::from_str(message)).unwrap();
+
+    func.call1(&global, &obj).unwrap();
 }
